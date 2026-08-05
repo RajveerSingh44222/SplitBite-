1b1bad6835fcdd5d03ffe1c312d6e3d76d7917af
RajveerSingh44222 <rajsisodia446@gmail.com>
2026-08-06 00:35:30 +0530

    --

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -33,3 +33,62 @@ def signup(
     return {
         "message": "Account created successfully"
     }
+
+@router.post("/login")
+def login(
+    request: LoginRequest,
+    db: Session = Depends(get_db)
+):
+    user = (
+        db.query(User)
+        .filter(User.email == request.email)
+        .first()
+    )
+
+    if not user or not verify_password(request.password, user.password_hash):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid Credentials"
+        )
+
+    access_token = create_access_token(data={"sub": user.id})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+@router.post("/refresh")
+def refresh_token(
+    current_user: User = Depends(get_current_refresh_user)
+):
+    access_token = create_access_token(
+        data={
+            "user_id": str(current_user.id),
+            "email": current_user.email
+        }
+    )
+
+    refresh_token = create_refresh_token(
+        data={
+            "user_id": str(current_user.id)
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer"
+    }
+
+@router.post("/logout")
+def logout(
+    current_user: User = Depends(get_current_refresh_user)
+):
+    delete_refresh_token(
+        user_id=str(current_user.id)
+    )
+
+    return {
+        "message": "Logged out successfully"
+    }
